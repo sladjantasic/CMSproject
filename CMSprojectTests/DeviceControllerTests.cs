f384ad886751b1c832b125d4aadb6d1fc5c9935a
sladjantasic <sladjan.m.tasic@gmail.com>
2020-07-03 10:07:58 +0200

    added test project

---
+++ CMSprojectTests/DeviceControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CMSprojectTests
+{
+    public class DeviceControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
