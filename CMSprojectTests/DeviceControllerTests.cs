172e3a07e63299b2a569374b6c7233a4c96958ed
sladjantasic <sladjan.m.tasic@gmail.com>
2020-07-03 10:48:18 +0200

    test setup

--- CMSprojectTests/DeviceControllerTests.cs
+++ CMSprojectTests/DeviceControllerTests.cs
@@ -1,3 +1,9 @@
+using AutoMapper;
+using CMSproject.Controllers;
+using CMSproject.Data;
+using CMSproject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
 
@@ -5,6 +11,18 @@ namespace CMSprojectTests
 {
     public class DeviceControllerTests
     {
+        private readonly DevicesController deviceController;
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+        private readonly UserManager<User> userManager;
+
+        public DeviceControllerTests(ApplicationDbContext context, IMapper mapper, UserManager<User> userManager)
+        {
+            this.context = context;
+            this.mapper = mapper;
+            this.userManager = userManager;
+            deviceController = new DevicesController(this.context, this.mapper, this.userManager);
+        }
         [Fact]
         public void Test1()
         {
